475f9f662b48ac22c219f15e392f989275ea0505
Jerry <creatmylife850710@gmail.com>
2019-02-17 02:12:57 +0800

    use timeout

--- run.sh
+++ run.sh
@@ -7,4 +7,13 @@ rm experiment/*
 
 #python dist/client/dash_client.py -m http://140.114.77.125/mpd_with_size/dash_diving_10x10_qp28_new.mpd -p 'netflix' -u 20 -s l
 
-timeout 5s python dist/client/dash_client.py -m http://140.114.77.125/mpd_with_size/dash_drive_10x10_qp28_new.mpd -p 'netflix' -u 35 -s l
+finish='false'
+
+while [ $finish == 'false' ]
+do
+rm experiment/*
+finish='true'
+gtimeout 70s python dist/client/dash_client.py -m http://140.114.77.125/mpd_with_size/dash_drive_10x10_qp28_new.mpd -p 'netflix' -u 35 -s l || finish='false'
+done
+
+echo "finish video"
