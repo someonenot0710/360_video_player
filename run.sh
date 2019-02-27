a403fabafb25eb6995b40c2f2491020988011f0f
Jerry <yenscj@gmail.com>
2019-02-27 12:40:47 +0800

    add running script

--- run.sh
+++ run.sh
@@ -1,11 +1,28 @@
 #python dist/client/dash_client.py -m http://140.114.77.125/coaster_10x10/dash_coaster_10x10_qp28_new.mpd -p 'basic'
-rm -r experiment/*
+#rm -r experiment/*
 
 #python dist/client/dash_client.py -m http://140.114.77.125/mpd_with_size/dash_coaster_10x10_qp28_new.mpd -p 'netflix' -u 30 -s l
 
 #dash_diving_10x10_qp28_new.mpd
 
-python3 dist/client/dash_client.py -m http://140.114.77.125/mpd_with_size/dash_diving_10x10_qp28_new.mpd -p 'netflix' -u 30 -s l -pro q -urgent y
+
+## original
+#python3 dist/client/dash_client.py -m http://140.114.77.125/mpd_with_size/dash_diving_10x10_qp28_new.mpd -p 'netflix' -u $1 -s s -pro q -urgent y -comp n
+
+
+# video , user
+python3 dist/client/dash_client.py -m http://140.114.77.125/mpd_server/dash_$1_10x10_qp28_new.mpd -p 'netflix' -u $2 -s s -pro q -urgent y -comp n
+
+
+
+
+
+
+
+
+
+
+#python3 dist/client/dash_client.py -m http://140.114.77.125/mpd_with_size/dash_diving_10x10_qp28_new.mpd -p 'netflix' -u 30 -s s -pro h2 -urgent n -comp y
 
 
 #finish='false'
---
+++ w.sh
@@ -0,0 +1,1 @@
+sudo tcpdump -i enp1s0f1 'src host 140.114.77.170' > dump
